b5de226fbd2262d029fe3bdd29e9066a87823f35
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-26 16:31:35 +0530

    [Devesh]: Added ability to create BST by several nodes and check nodes added or not using size feature

--- src/com/binarysearchtree/MyBinaryTreeRunner.java
+++ src/com/binarysearchtree/MyBinaryTreeRunner.java
@@ -1,12 +1,33 @@
 package com.binarysearchtree;
+import java.util.Scanner;
 
 public class MyBinaryTreeRunner {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
-		myBinaryTree.add(56);
-		myBinaryTree.add(30);
-		myBinaryTree.add(70);
+		int flag = 0;
+		while (true) {
+			System.out.println("Enter 1.Add 2.exit");
+			int option = sc.nextInt();
+			switch(option) {
+			case 1:
+				myBinaryTree.add(sc.nextInt());
+				break;
+				
+			case 2:
+				flag = 1;
+				break;
+				
+			default:
+				break;
+				
+			}
+			if (flag == 1) break;
+		}
+		System.out.println("The Size of Binary Tree is " + myBinaryTree.size());
+		System.out.println("Binary Tree Nodes: ");
 		myBinaryTree.printTree();
 	}
 }
